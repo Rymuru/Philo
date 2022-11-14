344a815836f8923cbd8eded4b663264172d99373
bcoenon <bcoenon@student.42.fr>
2022-11-14 19:10:00 +0100

    structure avec elements essentiels definis

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,11 +6,24 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 18:52:16 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/11/14 18:54:38 by bcoenon          ###   ########.fr       */
+/*   Updated: 2022/11/14 19:05:39 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
-#include <stdio.h>
-#include <sys/time.h>
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
 
+typedef struct s_philo
+{
+	pthread_mutex_t	fork;
+	int				time_to_eat;
+	int				time_to_die;
+	int				time_to_sleep;
+	int				lunches;
+}	t_philo;
+
+#endif
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,32 +6,29 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:29:20 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/11/14 18:51:43 by bcoenon          ###   ########.fr       */
+/*   Updated: 2022/11/14 19:09:04 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "include/philo.h"
 
 int	main(int ac, char **av)
 {
-	int	philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	lunches;
-	pthread_t 
+	int		philo;
+	t_philo	dio;
 
 	if (ac != 5 || ac != 6)
 		return (0);
 	philo = ft_atoi(av[1]);
-	time_to_die = ft_atoi(av[2]);
-	time_to_eat = ft_atoi(av[3]);
-	time_to_sleep = ft_atoi(av[4]);
+	dio.time_to_die = ft_atoi(av[2]);
+	dio.time_to_eat = ft_atoi(av[3]);
+	dio.time_to_sleep = ft_atoi(av[4]);
 	if (av[5])
-		lunches = ft_atoi(av[5]);
+		dio.lunches = ft_atoi(av[5]);
 	while (philo > 0)
 	{
-		pthread_create()
+		pthread_create();
+		--philo;
 	}
 }
 
