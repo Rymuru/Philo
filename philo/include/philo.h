8e1b6f3cb211df3289506870b5d7d69ffd785d85
bcoenon <bcoenon@student.42.fr>
2023-01-12 17:21:04 +0100

    start to invert struct implementation

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 18:52:16 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/12/30 12:59:50 by bcoenon          ###   ########.fr       */
+/*   Updated: 2023/01/12 16:36:14 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,33 @@
 # include <stdint.h>
 # include <limits.h>
 
-typedef struct s_philo
-{
-	int				thread_id;
-	int				lunches;
-	uint64_t		last_eat;
-	uint64_t		sleep;
-	uint64_t		time_to_eat;
-	uint64_t		time_to_die;
-	uint64_t		time_to_sleep;
-	pthread_mutex_t	fork;
-	pthread_mutex_t	eat;
-}	t_philo;
-
 typedef struct s_data
 {
 	int				philo;
 	int				death;
 	int				current;
 	int				lunches;
-	uint64_t		start;
 	uint64_t		time_to_eat;
 	uint64_t		time_to_die;
 	uint64_t		time_to_sleep;
-	t_philo			*ecclesia;
-	pthread_mutex_t	lock;
 	pthread_mutex_t	write;
-	pthread_mutex_t	clock;
+	pthread_mutex_t	lock;
 	pthread_mutex_t	watcher;
 }	t_data;
 
-uint64_t	ft_clock(t_data *data);
+typedef struct s_philo
+{
+	int				thread_id;
+	int				lunches;
+	t_data			*data;
+	uint64_t		last_eat;
+	uint64_t		sleep;
+	pthread_mutex_t	fork;
+}	t_philo;
+
+uint64_t	ft_clock(void);
 uint64_t	ft_atoi(char *str);
 int			is_all_num(char **str);
-int			is_someone_dead(t_data *data);
-int			live(t_data *data, t_philo *ari);
 int			init_data(t_data *data, char **av);
 int			check_death(t_data *data, t_philo *ari);
 
--- philo/srcs/live.c
+++ philo/srcs/live.c
@@ -6,94 +6,40 @@
 /*   By: bcoenon <bcoenon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 00:20:08 by bcoenon           #+#    #+#             */
-/*   Updated: 2022/12/30 13:01:27 by bcoenon          ###   ########.fr       */
+/*   Updated: 2023/01/12 16:45:59 by bcoenon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	get_forks_pair(t_data *data, t_philo *ari)
+int	live(t_philo *ari)
 {
-	pthread_mutex_lock(&ari->fork);
-	protect_print(data, ari->thread_id, "has taken a fork");
-	if (data->philo == 1)
+	check_death(ari->data, ari);
+	if (ari->thread_id % 2 == 0 != ari->lunches != 0)
+		//lunch(data, ari);
+	if (ari->data->death == 0)
 	{
-		return (1);
-	}
-	else
-	{
-		if (ari->thread_id == 0 && data->philo != 1)
-			pthread_mutex_lock(&data->ecclesia[data->philo - 1].fork);
-		else if (data->philo != 1)
-			pthread_mutex_lock(&data->ecclesia[ari->thread_id - 1].fork);
-		protect_print(data, ari->thread_id, "has taken a fork");
-	}
-	return (0);
-}
-
-int	get_forks_impair(t_data *data, t_philo *ari)
-{
-	if (ari->thread_id == 0)
-		pthread_mutex_lock(&data->ecclesia[data->philo - 1].fork);
-	else
-		pthread_mutex_lock(&data->ecclesia[ari->thread_id - 1].fork);
-	protect_print(data, ari->thread_id, "has taken a fork");
-	pthread_mutex_lock(&ari->fork);
-	protect_print(data, ari->thread_id, "has taken a fork");
-	return (0);
-}
-
-int	get_forks(t_data *data, t_philo *ari)
-{
-	if (ari->thread_id % 2 == 2)
-	{
-		return (get_forks_pair(data, ari));
-	}
-	else
-	{
-		return (get_forks_impair(data, ari));
-	}
-}
-
-int	lunch(t_data *data, t_philo *ari)
-{
-	if (get_forks(data, ari) == 0 && is_someone_dead(data) == 0)
-	{
-		protect_print(data, ari->thread_id, "is eating");
-		pthread_mutex_lock(&ari->eat);
-		ari->last_eat = ft_clock(data);
-		++ari->lunches;
-		pthread_mutex_unlock(&ari->eat);
-		while (ft_clock(data) - ari->last_eat <= ari->time_to_eat
-			&& is_someone_dead(data) == 0)
-			usleep(1);
-	}
-	else
-	{
-		pthread_mutex_unlock(&ari->fork);
-		usleep(ari->time_to_die);
-		return (1);
-	}
-	pthread_mutex_unlock(&ari->fork);
-	if (ari->thread_id == 0 && data->philo != 1)
-		pthread_mutex_unlock(&data->ecclesia[data->philo - 1].fork);
-	else if (data->philo != 1)
-		pthread_mutex_unlock(&data->ecclesia[ari->thread_id - 1].fork);
-	return (0);
-}
-
-int	live(t_data *data, t_philo *ari)
-{
-	if (lunch(data, ari) == 0 && is_someone_dead(data) == 0)
-	{
-		ari->sleep = ft_clock(data);
-		protect_print(data, ari->thread_id, "is sleeping");
-		while (ft_clock(data) - ari->sleep <= ari->time_to_sleep)
+		ari->sleep = ft_clock();
+		protect_print(ari->data, ari->thread_id, "is sleeping");
+		while (ft_clock() - ari->sleep <= ari->data->time_to_sleep)
 		{
-			if (is_someone_dead(data) == 1)
-				return (1);
+			if (ft_clock() - ari->last_eat > ari->data->time_to_die)
+				check_death(ari->data, ari);
 		}
-		protect_print(data, ari->thread_id, "is thinking");
+		protect_print(ari->data, ari->thread_id, "is thinking");
 	}
 	return (0);
+}
+
+void	*routine(void *arg)
+{
+	int		i;
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	philo->last_eat = ft_clock();
+	while (philo->data->death == 0 && philo->lunches != philo->data->lunches)
+		live(philo);
+ 	philo->data->death = 1;
+	return (arg);
 }
